2b41d494cc4d459eb7efaebb5141a5e27ba9b32e
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-08-28 19:51:47 +0530

    completed day 1 tasks

--- Day1/factorialN.cpp
+++ Day1/factorialN.cpp
@@ -2,7 +2,28 @@
 using namespace std;
 
 int main()
+
 {
+    // Factorial of N no's
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Enter a number greater than 0...";
+        return 0;
+    }
+
+    long long factorial = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+
+    cout << "Factorial of " << n << " is " << factorial;
 
     return 0;
 }
