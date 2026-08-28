7d53cb066830ac5190c28537abc41876b0733de1
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-08-28 11:20:57 +0530

    build a simple calculator

--- Day0/Calculator.cpp
+++ Day0/Calculator.cpp
@@ -3,8 +3,52 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World\n"
-            "Hello from c++\n"
-            "Hey there";
+    // Simple Calculator
+    int num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operator (+, -, *, /,%): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (op)
+    {
+    case '+':
+        cout << "Result: " << num1 + num2;
+        break;
+
+    case '-':
+        cout << "Result: " << num1 - num2;
+        break;
+
+    case '*':
+        cout << "Result: " << num1 * num2;
+        break;
+
+    case '%':
+        cout << "Result: " << num1 % num2;
+        break;
+
+    case '/':
+        if (num2 != 0)
+        {
+            cout << "Result: " << num1 / num2 << endl;
+        }
+        else
+        {
+            cout << "Error: Division by zero is not allowed." << endl;
+        }
+        break;
+
+    default:
+        cout << "Error: Invalid operator!" << endl;
+        break;
+    }
+
     return 0;
 }
