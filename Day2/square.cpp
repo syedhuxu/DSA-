513641e6227f519a635b21e62de2efce0303d593
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-08-29 20:05:06 +0530

    Practicing patterns

---
+++ Day2/square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 4;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
