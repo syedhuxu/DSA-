84f30cd6f9eb56541106881f5ca1d11f4e140a9f
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-09-04 15:32:12 +0530

    Practicing triangle  patterns

---
+++ Day3/alphabetTriangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch = 'A';
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << ch << " ";
+        }
+        ch++;
+        cout << endl;
+    }
+    return 0;
+}
