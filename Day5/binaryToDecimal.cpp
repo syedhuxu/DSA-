0b40707e8469829fa4c63706f16ab0ff880af632
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-09-05 19:49:53 +0530

    binary to decimal

---
+++ Day5/binaryToDecimal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int binToDec(int n)
+{
+    int ans = 0, pow = 1;
+
+    while (n > 0)
+    {
+        int rem = n % 10;
+        ans += (rem * pow);
+        n /= 10;
+        pow *= 2;
+    }
+    return ans;
+}
+int main()
+{
+
+    int result = binToDec(101101);
+    cout << (1 / 10);
+    cout << result;
+    return 0;
+}
