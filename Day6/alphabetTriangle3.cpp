a5579c6685ebb8320ff52f0098c2c9dacadad7f3
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-09-06 15:38:06 +0530

    reverse triangle kind of something ...

---
+++ Day6/alphabetTriangle3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        // loop for spaces
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = n - i; k > 0; k--)
+        {
+            cout << char('A' + i);
+        }
+        cout << endl;
+    }
+    return 0;
+}
