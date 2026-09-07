b937415578cdf364a79d111cad05f47dcf92aa75
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-09-07 20:03:10 +0530

    IsPowerOf2

---
+++ Day7/isPowerOf2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 32;
+    if (n > 0 && (n & (n - 1)) == 0)
+        cout << "Power of 2";
+    else
+        cout << "Not a power of 2";
+    return 0;
+}
