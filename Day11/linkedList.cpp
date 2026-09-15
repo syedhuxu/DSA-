606cce07f3870e6c8ebe474f0874d48f6edc050e
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-09-15 20:02:13 +0530

    Introduction to Linked Lists

--- Day11/linkedList.cpp
+++ Day11/linkedList.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Node
+struct Node
 {
     int data;
     Node *next;
@@ -21,10 +21,39 @@ public:
     {
         head = tail = NULL;
     }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    void printLL()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };
 
-main()
+int main()
 {
-    cout << "Hello World";
+    List ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.printLL();
     return 0;
 }
